45d6f2a1db4f12a28c340b16ebc6fbbe9734b310
nikesh <nikesh.strat@yahoo.com>
2017-11-07 17:34:18 +0000

    cleanup

--- scripts/wts_ft_listener.py
+++ scripts/wts_ft_listener.py
@@ -1,55 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-:ABSTRACT:
-
-
-:REQUIRES:
-
-:TODO:
-
-:AUTHOR:  Pedro Machado
-:ORGANIZATION: Nottingham Trent University
-:CONTACT: pedro.baptistamachado@ntu.ac.uk
-:SINCE: Thu Jul  6 17:10:26 2017
-:VERSION: 0.1
- 
-This file is part of icub-cncr-ntu project.
-icub-cncr-ntu project can not be copied and/or distributed without the express
-permission of Prof. Martin McGinnity <martin.mcginnity@ntu.ac.uk>
-
-Copyright (C) 2017 All rights reserved, Nottingham Trent University
-Computational Neuroscience and Cognitive Robotics Laboratory
-email:   pedro.baptistamachado@ntu.ac.uk
-website: https://www.ntu.ac.uk/research/groups-and-centres/groups/computational-neuroscience-and-cognitive-robotics-laboratory
-
-
-"""
-#===============================================================================
-# PROGRAM METADATA
-#===============================================================================
-__author__ = 'Pedro Machado'
-__contact__ = 'pedro.baptistamachado@ntu.ac.uk'
-__copyright__ = 'iCub_cncr_ntu project can not be copied and/or distributed \
-without the express permission of Prof. Martin McGinnity <martin.mcginnity@ntu.ac.uk'
-__license__ = 'Copyright (C)'
-__date__ = 'Thu Jul  6 17:10:26 2017'
-__version__ = '0.1'
-__file_name__='<FILE>.py'
-__description__='DESC'
-#===============================================================================
-# IMPORT STATEMENTS
-#===============================================================================
-
-
-#===============================================================================
-# METHODS
-#===============================================================================
-
-
-#===============================================================================
-# MAIN METHOD AND TESTING AREA
-#===============================================================================
 
 import rospy
 from rospy_tutorials.msg import Floats
@@ -76,34 +26,18 @@ def callback(data):
 		#f.close()  
 		#print('Raw array: {}'.format(array))      
 		array_fix=array.reshape((wtsft.SX,wtsft.SY))
-		#print array_fix
-		#print np.shape(array_fix)
-		#im = plt.imshow(array_fix)
-		#im = plt.pcolor(array_fix, cmap = plt.cm.Reds)
+		#updating the array for heatmap
 		im.set_data(array_fix)
-		#ax.set_facecolor('black')
-		#for (i, j), z in np.ndenumerate(array_fix):
-		#	ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
-		#im = plt.pcolor(array_fix, cmap = plt.cm.Reds)
-		#plt.text(array_fix,  horizontalalignment='center', verticalalignment='center')
-        
-		#ax.invert_yaxis()
-		#plt.gca().set_aspect('equal', adjustable='box')
 		plt.draw()
 		if(np.amax(array_fix) > int(62000)):
-			
+			#just a friendly message
 			plt.title("Too strong", color = 'red')
 			print("Too strong!!!!!!!!!!!!!!!!!!!!!")
 		else:
 			plt.title("Reading Pressure", color = 'white')
 		#ax.cla()
-        
-'''              
-def updatefig(*args):
-    
-    im.set_array(array_fix)
-    return im,      
-'''
+#main listening function 
+#the sensro readings are published in wtsft topic       
 def listener():
 	rospy.init_node('listener')
 	rospy.Subscriber('wtsft', numpy_msg(Floats), callback)
@@ -116,12 +50,7 @@ if __name__ == '__main__':
 	im = ax.imshow(array_fix, interpolation = "nearest", cmap = cmaps.plasma,vmax = 80000)
 	#ax.set_facecolor('black')
 	fig.patch.set_facecolor('black')
-	#for (i, j), z in np.ndenumerate(array_fix):
-		#ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
-	#im = plt.pcolor(array_fix, cmap = plt.cm.Reds, animated = True)
-	#ax.grid(color = 'w', linestyle = '-', linewidth = 1)
-	#ax.xaxis.set_xticks(np.arange(0,4,1))
-	# Major ticks
+	#setting grid like structure
 	ax.set_xticks(np.arange(0, 4, 1));
 	ax.set_yticks(np.arange(0, 8, 1));
 
