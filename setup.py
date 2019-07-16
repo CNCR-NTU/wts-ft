5f1678f32ac8b0d1525ef05d637f016dbb718020
pedrombmachado <pedro.baptistamachado@ntu.ac.uk>
2019-07-16 16:00:06 +0100

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-	packages=['object_detection'],
+	packages=['wts_ft'],
 	package_dir={'':'src'},
+	requires=['std_msgs', 'rospy'],
 )
 
 
